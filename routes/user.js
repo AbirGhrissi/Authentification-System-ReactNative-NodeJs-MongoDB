99861a1b659a9010797cc8b0366f9b4b2b89e2c8
AbirGhrissi <abirghrissi83@gmail.com>
2025-01-08 12:49:20 +0100

    feat: add upload image file

--- routes/user.js
+++ routes/user.js
@@ -6,11 +6,26 @@ const { createUser, userSignIn } = require('../controllers/user');
 const { validateUserSignUp, userValidation, validateUserSignIn } = require('../middleware/validation/user');
 const { isAuth } = require('../middleware/auth');
 
+const multer = require('multer');
+
+const storage = multer.memoryStorage();
+
+const fileFilter = (req, file, cb) =>{
+    if(file.mimetype.startswith('image')){
+        cb(null, true);
+    }else{
+        cb('invalid image file', false);
+    }
+}
+
+const uploads = multer({storage,fileFilter})
+
 router.post('/create-user',validateUserSignUp,userValidation,createUser)
 router.post('/sign-in', validateUserSignIn, userValidation, userSignIn)
-router.post('/upload-profile', isAuth,(req,res) => {
+router.post('/upload-profile', isAuth,uploads.single('profile'), (req,res) => {
     const {user } = req
     if (!user) return res.status(401).json({success: false, message: 'Unauthorized access !'})
+    req.file
 
 })
 
