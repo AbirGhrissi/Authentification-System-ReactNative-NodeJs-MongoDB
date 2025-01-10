296531ca17ff65704a1fc0867163fa3b57c23695
AbirGhrissi <abirghrissi83@gmail.com>
2025-01-10 17:15:15 +0100

    feat: initial frontend commit

---
+++ FrontEnd/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import FormHeader from './app/components/FormHeader';
+
+export default function App() {
+  return (
+    <View style={{flex: 1, paddingTop: 120}}>
+      <View style={{height:100}}>
+        <FormHeader leftHeading='Welcome ' rightHeading='Back' subHeading='YouTube Task Manager'/>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
